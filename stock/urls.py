3d136dd6762643c8fa85f352fc4fb3392769f6fd
nomadlife15u <aaeacd@gmail.com>
2018-11-27 13:48:53 +0900

    stock pagination, dropdown, test link added

--- stock/urls.py
+++ stock/urls.py
@@ -3,10 +3,19 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.home, name='stock-list'),
+    url(r'^$', views.home),
     url(r'^hello/$', views.hello_fn, name='stock-hello'),
-    url(r'^home2/$', views.HomePageView.as_view(), name='stock-home2'),
-    url(r'^list/$', views.stock_list3, name='stock-home'),
 
+    url(r'^cbvtest/$', views.HomePageView.as_view()),
+
+    url(r'^list/$', views.stock_list),
+    url(r'^list2/$', views.stock_list2),
+    url(r'^list3/$', views.stock_list3),
+    url(r'^list4/$', views.stock_list_page, name='stock-home'),
+
+    url(r'^list5/$', views.StockListPage.as_view()),
+    url(r'^list6/$', views.StockListPage2.as_view()),
+
+    url(r'^search/(?P<ticker_id>.+?)/', views.test_search, name='ticker-search'),
 
 ]
