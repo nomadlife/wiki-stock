1ebf0a281ea6858ab99176065da2d86e469753ec
nomadlife15u <aaeacd@gmail.com>
2018-11-27 16:46:06 +0900

    detail view changed

--- stock/views.py
+++ stock/views.py
@@ -9,6 +9,10 @@ from django.shortcuts import render
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from django.contrib.staticfiles.templatetags.staticfiles import static
 
+import pandas as pd
+import pandas_datareader.data as web
+from datetime import datetime,date
+
 # Create your views here.
 def home(request):
     return render(request, 'stock/home.html')
@@ -68,12 +72,16 @@ class StockListPage2(View):
 def test_search(request, ticker_id):
     file = open(os.path.join(base.BASE_DIR, 'stock/statics/stock/alltickers_2018.csv'))
     data = [i.split(',') for i in file.readlines()]
-    temp = []
+    info = []
     for j in data:
         if j[0] == ticker_id:
-            temp = j
-
-    context = {'ticker_code':temp[2].zfill(6), 'data':temp}
+            info = j
+    today = date.today()
+    start = datetime(2018, 11, 1)
+    end = datetime(today.year, today.month, today.day)
+    history_data = web.DataReader(info[2].zfill(6)+'.KS','yahoo',start, end)
+    # history_data_new = history_data.sort_index(inplace=True, ascending=False)
+    context = {'ticker_code':info[2].zfill(6), 'data':info, 'history_data':history_data}
     return render(request, 'stock/test_search.html', context )
 
 
